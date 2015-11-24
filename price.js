2d83dd345752cd208abe8498212f44d0a7a35f0f
larry <larry@study64>
2015-11-24 08:40:23 +0800

    yql

--- price.js
+++ price.js
@@ -25,22 +25,26 @@ new (function() {
               url: 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quote%20where%20symbol%20%3D%20"' + symbol + '"&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys',
               dataType: 'jsonp',
               success: function( json_data ) {
-                  // Got the data - parse it and return the value
-		  switch (priceType){
-			case 'Hi':
-	                	price = json_data['query']['results']['quote']['DaysHigh'];
-				break;
-			case 'Lo':
-	                	price = json_data['query']['results']['quote']['DaysLow'];
-				break;
-			case 'Last':
-	                	price = json_data['query']['results']['quote']['LastTradePriceOnly'];
-				break;
-			
-		  }
 
-		  if (price == null)
+		  try{
+                  // Got the data - parse it and return the value
+			  switch (priceType){
+				case 'Hi':
+			        	price = json_data['query']['results']['quote']['DaysHigh'];
+					break;
+				case 'Lo':
+			        	price = json_data['query']['results']['quote']['DaysLow'];
+					break;
+				case 'Last':
+			        	price = json_data['query']['results']['quote']['LastTradePriceOnly'];
+					break;
+				default:
+					price = 0;
+			
+			  }
+		  } catch (err){
 			 price = 0; 
+		  }
                   callback(price);
               }
 
@@ -52,11 +56,11 @@ new (function() {
     // Block and block menu descriptions
     var descriptor = {
         blocks: [
-            ['r', 'current %m.priceType price of %s', 'get_px', 'Last', '^HSI'],
+            ['r', 'current %m.priceType price of %s', 'get_px', 'Last', '^HSI']
         ],
 
 	menus: {
-        	priceType: ['Hi', 'Lo', 'Last' ],
+        	priceType: ['Hi', 'Lo', 'Last' ]
         	
     	}
     };
